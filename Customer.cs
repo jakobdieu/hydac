bbd141da2dd38182201951c46df9f8d9324ff4c1
jakobdieu <152283959+jakobdieu@users.noreply.github.com>
2024-03-13 20:03:04 +0100

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace hydac_customer
+{
+	public class Customer
+	{
+        public string name;
+        public string CVR;
+		//public Location location;
+
+        public Customer(string name, string CVR)
+		{
+			name = name;
+			CVR = CVR;
+
+		}
+		public string GetName()
+		{
+			return name;
+
+
+		}
+		public void SetName(string NewName)
+		{
+			name = NewName;
+		}
+
+		public string GetCVR()
+		{
+			return CVR;
+
+		}
+		public void SetCVR(string NewCVR)
+		{
+			CVR = NewCVR;
+		}
+
+
+
+
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace hydac_customer
+{
+	public class Weather
+	{
+		public float Temperature;
+		public float Humidity;
+		public float Precipitation;
+		public DateTime MeasurementDate;
+
+		public Weather(float Temperature, float Humidity, float Precipitation, DateTime MeasurementDat)
+		{
+			Temperature = Temperature;
+			Humidity = Humidity;
+			Precipitation = Precipitation;
+			MeasurementDate = MeasurementDate;
+		}
+		public float GetTemperature()
+		{
+			return Temperature;
+		}
+		public void SetTemperature(float NewTemperature)
+		{
+			Temperature = NewTemperature;
+		}
+
+		public float GetHumidity()
+		{
+			return Humidity;
+		}
+		public void SetHumidity(float NewHumdity)
+		{
+			Humidity = NewHumdity;
+		}
+
+		public float GetPrecipitation()
+		{
+			return Precipitation;
+		}
+		public void SetPrecipitation(float NewPrecipitation)
+		{
+			Precipitation = NewPrecipitation;
+		}
+
+	}
+}
+
